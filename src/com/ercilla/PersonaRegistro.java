2b40c0a4ecce3cfe2d2196a428f6a059a08f4488
hugogomezfernandez100-eng <hugogomezfernandez100@gmail.com>
2025-09-23 12:09:25 +0200

    Comentario sobre codigo inservible

--- src/com/ercilla/PersonaRegistro.java
+++ src/com/ercilla/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.ercilla;
 public record PersonaRegistro(String nombre, String apellido,) {
 
     public String toString() {
+        //Pepito comento la linea de codigo que se puede eliminar 
         System.out.println("Esta instruccion es inservible");
         return String.format("Nombre: %s, Apellido: %s", this.nombre, this.apellido);
     }
